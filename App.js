85a0c001d35c979a47a7c26c152a21db2f7b6b15
Hifzur Rehman Alauddin khan <hifzur.khan@admins-MacBook-Pro.local>
2022-12-12 12:42:50 +0530

    url set

--- App.js
+++ App.js
@@ -1,1 +1,36 @@
-console.log("hello");
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+// app.use(authJwt());
+
+//Routes
+
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log("err",err);
+  });
+
+//Server
+app.listen(process.env.PORT, () => {
+  console.log(`server is running ${process.env.PORT}`);
+});
